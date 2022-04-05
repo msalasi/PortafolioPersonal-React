d42c1a06b1fbe6e5309877bab1b066eb2578060c
msalasi <salasmichael111@gmail.com>
2022-04-04 20:13:36 -0400

    respaldo final con responsive incluido

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import Header from "./components/Header/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './components/Header/Header.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './views/Home/Home.js';
 import Works from './views/Works/Works.js';
-import Footer from "./components/Footer/Footer";
+import Footer from './components/Footer/Footer.js';
+
 
 
 function App() {
+  
+
   return (
     <>
-       <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/works" element={<Works />} />
-        </Routes>
-      </Router>
-      <Footer/>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/works" element={<Works />} />
+          </Routes>
+        </Router>
+        <Footer/>
     </>
+ 
+    
      
   );
 }
