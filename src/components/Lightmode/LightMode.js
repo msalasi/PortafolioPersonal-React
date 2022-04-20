bfe36d74341778cffc8570500fe308cbf36d6e15
msalasi <salasmichael111@gmail.com>
2022-04-19 23:08:10 -0400

    modificaciones en light mode

--- src/components/Lightmode/LightMode.js
+++ src/components/Lightmode/LightMode.js
@@ -2,24 +2,26 @@ import React from 'react'
 import './LightMode.css';
 
 function LightMode() {
-            
-                const darkCheck = document.getElementById('flexSwitchCheckChecked');
-                if (darkCheck.checked) {
-                    document.body.classList.add('light');
-                    localStorage.setItem('setTheme', 'light');
-                    console.log(localStorage.getItem('setTheme'));
-                    
-                } else {
-                    document.body.classList.remove('light');
-                    localStorage.removeItem('setTheme');
-                    console.log("eliminado del localStorage");
-                }
-        
-                if (localStorage.getItem('setTheme')) {
-                    document.body.className = 'light';
-                    darkCheck.checked = true;
-                }
-            
+    const darkCheck = document.getElementById('flexSwitchCheckChecked');
+    if (darkCheck.checked) {
+        document.body.classList.add('light');
+        localStorage.setItem('setTheme', 'light');
+        console.log("Se cambio a ", localStorage.getItem('setTheme'));
+    } else {
+        document.body.classList.remove('light');
+        localStorage.removeItem('setTheme');
+        console.log("Se cambio a ", localStorage.getItem('setTheme'));
+    }
+
+
+    if (localStorage.getItem('setTheme')) {
+        console.log("existe el local");
+        document.body.className = 'light';
+        darkCheck.checked = true;
+    }else{
+        console.log("no existe el local");
+    }
+
     return (
         <div></div>
     )
