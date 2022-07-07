ef76e887ace22f5a42fe1e3ee69695ed20b21450
msalasi <salasmichael111@gmail.com>
2022-07-06 23:09:52 -0400

    i add new project in my works section (link in bio for instagram)

--- src/views/Works/Works.js
+++ src/views/Works/Works.js
@@ -1,32 +1,70 @@
-import React from 'react'
-import TodoAppImg from '../../assets/Img/TodoApp.png';
-import CovidTracker from '../../assets/Img/covidtracker-modded.png';
-import './Works.css';
-import '../../components/Lightmode/LightMode.css';
+import React from "react";
+import TodoAppImg from "../../assets/Img/TodoApp.png";
+import CovidTracker from "../../assets/Img/covidtracker-modded.png";
+import LinkInBio from "../../assets/Img/Linkinbiomodded.png";
+import "./Works.css";
+import "../../components/Lightmode/LightMode.css";
 
 const Works = () => {
   return (
-  <>
-    <article className='article-projects'>
-      <div className='main-container-projects'>
-        <h3 className='h3-projects'>My Works</h3>
-        <div className='row-1-works'>
-          <div className='work'>
-            <img className='img-todo-app' src={TodoAppImg} alt='Todo App'></img>
-            <a className='work-h2' href="https://todoaplication.netlify.app/" target="_blank" rel="noreferrer">Todo App</a>
-            <p className='work-p'>Aplication for add or remove works</p>
-          </div>
-          <div className='work'>
-            <img className='img-todo-app' src={CovidTracker} alt='Covid Tracker'></img>
-            <a className='work-h2' href="https://covidtracker-g.netlify.app/" target="_blank" rel="noreferrer">Covid Tracker</a>
-            <p className='work-p'>Covid tracker aplication</p>
+    <>
+      <article className="article-projects">
+        <div className="main-container-projects">
+          <h3 className="h3-projects">My Works</h3>
+          <div className="row-1-works">
+            <div className="work">
+              <img
+                className="img-todo-app"
+                src={TodoAppImg}
+                alt="Todo App"
+              ></img>
+              <a
+                className="work-h2"
+                href="https://todoaplication.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Todo App
+              </a>
+              <p className="work-p">Aplication for add or remove works</p>
+            </div>
+            <div className="work">
+              <img
+                className="img-todo-app"
+                src={CovidTracker}
+                alt="Covid Tracker"
+              ></img>
+              <a
+                className="work-h2"
+                href="https://covidtracker-g.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Covid Tracker
+              </a>
+              <p className="work-p">Covid tracker aplication</p>
+            </div>
+            <div className="work">
+              <img
+                className="img-todo-app"
+                src={LinkInBio}
+                alt="Covid Tracker"
+              ></img>
+              <a
+                className="work-h2"
+                href="https://personal-link-in-bio.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Link in bio
+              </a>
+              <p className="work-p">Personal link in bio for Instagram</p>
+            </div>
           </div>
         </div>
-      </div>
-    </article>
-  </>
-    
-  )
-}
+      </article>
+    </>
+  );
+};
 
-export default Works
+export default Works;
